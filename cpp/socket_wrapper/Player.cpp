4cec366c0d6d9a9aafc15e1b2601be2a0f80f8e2
Christopher Rossmith <echross@esekits1068.(none)>
2012-03-27 14:04:36 +0200

    Tweaked the makefile and removed the server. Added a main.

---
+++ cpp/socket_wrapper/Player.cpp
@@ -0,0 +1,25 @@
+#include "ClientSocket.hh"
+#include "SocketException.hh"
+#include <iostream>
+#include <string>
+
+//#define SERVER_IP "130.100.97.29"
+#define SERVER_IP "134.138.79.101"
+#define SERVER_PORT 50001
+
+int main (int argc, char *argv[])
+{
+   std:: string reply;
+   try
+   {
+      sockwrpr::ClientSocket client_socket(SERVER_IP, SERVER_PORT);
+      client_socket << "foo";
+      client_socket >> reply;
+      std::cout << reply;
+   }
+   catch (sockwrpr::SocketException &e)
+   {
+      std::cout << "Exception was caught:" << e.description() << "\n";
+   }
+   return 0;
+}
